86dd30aa3aab89a16d9964cce78c33aa0107119b
bogdanMatiukha <bogdanmatiukha@gmail.com>
2026-09-17 12:48:46 +0300

    Add code

--- ProgramLabs.cpp
+++ ProgramLabs.cpp
@@ -1,20 +1,68 @@
-// ProgramLabs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double distance(double x1, double y1, double z1,
+    double x2, double y2, double z2)
+{
+    return sqrt(
+        pow(x2 - x1, 2) +
+        pow(y2 - y1, 2) +
+        pow(z2 - z1, 2)
+    );
+}
+
+double inputNumber()
+{
+    double number;
+
+    while (!(cin >> number))
+    {
+        cout << "Invalid input! Please enter a number: ";
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+
+    return number;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double xA1, yA1, zA1;
+    double xB1, yB1, zB1;
+    double xC1, yC1, zC1;
+    double xA2, yA2, zA2;
+
+    cout << "Enter coordinates of A1:" << endl;
+    xA1 = inputNumber();
+    yA1 = inputNumber();
+    zA1 = inputNumber();
+
+    cout << "Enter coordinates of B1:" << endl;
+    xB1 = inputNumber();
+    yB1 = inputNumber();
+    zB1 = inputNumber();
+
+    cout << "Enter coordinates of C1:" << endl;
+    xC1 = inputNumber();
+    yC1 = inputNumber();
+    zC1 = inputNumber();
+
+    cout << "Enter coordinates of A2:" << endl;
+    xA2 = inputNumber();
+    yA2 = inputNumber();
+    zA2 = inputNumber();
+
+    double a = distance(xA1, yA1, zA1, xB1, yB1, zB1);
+    double b = distance(xB1, yB1, zB1, xC1, yC1, zC1);
+    double c = distance(xA1, yA1, zA1, xA2, yA2, zA2);
+
+    double total_length = 4 * (a + b + c);
+
+    cout << fixed << setprecision(2);
+    cout << "Total length of all edges = " << total_length << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
